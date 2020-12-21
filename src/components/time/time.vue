1ae96b68393df4c58795d653e8e1c9457bdf1207
673317908Liyi <liy@sayingdata.com>
2020-12-21 15:42:44 +0800

    制作实时、公共标题组件

---
+++ src/components/time/time.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="now_time">{{ timeId }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      timeId: null,
+    };
+  },
+  created() {},
+  mounted() {
+    // this.getDate()
+  },
+  destroyed() {
+    clearTimeout(this.timeId);
+  },
+  computed: {},
+  methods: {
+    getTime(timeStamp) {
+      var date = new Date(timeStamp);
+      var Y = date.getFullYear();
+      var M = date.getMonth() > 9 ? date.getMonth() : "0" + date.getMonth();
+      var D = date.getDate() > 9 ? date.getDate() : "0" + date.getDate();
+      var H = date.getHours() > 9 ? date.getHours() : "0" + date.getHours();
+      var m = date.getMinutes() > 9 ? date.getMinutes() : "0" + date.getMinutes();
+      var s = date.getSeconds() > 9 ? date.getSeconds() : "0" + date.getSeconds();
+      return Y + "-" + M + "-" + D + " " + H + ":" + m + ":" + s;
+    },
+    getDate() {
+      this.timeId = this.getTime(new Date());
+      setInterval(this.getDate, 1000);
+      clearInterval(this.getDate);
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+.now_time {
+  font-size: 15px;
+  margin-left: 10px;
+}
+</style>
---
+++ src/components/title/title.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="title">
+      <span>{{ title }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      default: "",
+      type: String,
+    },
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+.title {
+  color: white;
+  position: relative;
+  span {
+    margin-left: 10px;
+  }
+  &::before {
+    height: 30px;
+    width: 8px;
+    background:#69ddff;
+    content: "";
+    display: block;
+    position: absolute;
+    left: -10px;
+    top: 50%;
+    transform: translateY(-50%);
+    border-radius: 20px;
+  }
+}
+</style>
