e2988066f5a71d9117f7f848de4ede424d70316a
673317908Liyi <liy@sayingdata.com>
2020-12-22 16:48:20 +0800

    完善websocket代码

--- src/utils/socket_service.js
+++ src/utils/socket_service.js
@@ -8,26 +8,46 @@ export default class SocketService {
     }
     ws = null
     connected = false
+    sendRetryCount = 0
+    callBackMapping = {}
     connect() {
         if (!window.WebSocket) {
             return console.log('浏览器不支持WebSocket')
         }
-        this.ws = new WebSocket("ws://123.207.136.134:9010/ajaxchattest")
+        this.ws = new WebSocket("ws://47.106.75.208:9502")
         this.ws.onopen = () => {
             console.log('连接服务端成功')
             this.connected = true
         }
         this.ws.onclose = () => {
             console.log('连接服务端失败')
+            this.connected = false
         }
         this.ws.onmessage = (msg) => {
             console.log('接受数据')
-            console.log(msg)
+            const recvData = JSON.parse(msg.data)
+            const socketType = recvData.tag
+            if (this.callBackMapping[socketType]) {
+                const realData = recvData.data
+                this.callBackMapping[socketType].call(this, realData)
+            }
         }
+    }
+    registerCallBack(socketType, callBack) {
+        this.callBackMapping[socketType] = callBack
+    }
+    unRegisterCallBack(socketType) {
+        this.callBackMapping[socketType] = null
     }
     send(data) {
         if (this.connected) {
-            this.ws.send(data)
+            this.sendRetryCount = 0
+            this.ws.send(JSON.stringify(data))
+        } else {
+            this.sendRetryCount++
+            setTimeout(() => {
+                this.ws.send(JSON.stringify(data))
+            }, this.sendRetryCount * 500);
         }
     }
 }
